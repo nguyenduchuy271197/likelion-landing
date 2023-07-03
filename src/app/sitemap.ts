65bdac2f9f514b02c2e278c135d37a22eb333392
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-07-03 17:09:48 +0700

    new update

---
+++ src/app/sitemap.ts
@@ -0,0 +1,19 @@
+import { courses } from "@/components/layouts/navbar/Navbar";
+import siteConfig from "@/config/siteConfig";
+import { getBlogSlugsFromGithubRepo } from "@/services/blogService";
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const staticRoutes = ["", "/faq", "/about", "/contact"];
+
+  const courseRoutes = courses.map((c) => `/courses/${c.slug}`);
+
+  const blogRoutes = (await getBlogSlugsFromGithubRepo()).map(
+    (slug) => "/" + slug
+  );
+
+  return [...staticRoutes, ...courseRoutes, ...blogRoutes].map((r) => ({
+    url: `${siteConfig.url}${r}`,
+    lastModified: new Date(),
+  }));
+}
--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -3,6 +3,7 @@ const siteConfig = {
   description:
     "Khoá học lập trình offline từ con số 0 - LikeLion là một chương trình đào tạo chuyên sâu về lập trình, nhằm giúp bạn có thể bắt đầu từ con số 0 để trở thành một lập trình viên chuyên nghiệp. Chương trình được thiết kế bởi LikeLion - một cộng đồng lập trình viên có uy tín và kinh nghiệm trong lĩnh vực này.",
   brand: "LIKELION",
+  url: "https://www.likelion.edu.vn",
 };
 
 export default siteConfig;
