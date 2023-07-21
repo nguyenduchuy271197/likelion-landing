6145b5eadc367e72de32949995b4f16573643a05
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-07-21 14:43:08 +0700

    og

--- src/app/courses/[courseSlug]/page.tsx
+++ src/app/courses/[courseSlug]/page.tsx
@@ -12,20 +12,31 @@ import CourseRequirements from "@/components/courses/CourseRequirements";
 import CourseReviews from "@/components/courses/CourseReviews";
 import { courses } from "@/components/layouts/navbar/Navbar";
 import { getCourseBySlug } from "@/services/courseService";
-import { Metadata } from "next";
+import { Metadata, ResolvingMetadata } from "next";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { courseSlug: string };
-}): Promise<Metadata> {
+export async function generateMetadata(
+  {
+    params,
+  }: {
+    params: { courseSlug: string };
+  },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
   const { title, subtitle, thumbnail } = await getCourseBySlug(
     params.courseSlug
   );
 
+  const previousImages = (await parent).openGraph?.images || [];
+
   return {
     title: `${title}`,
     description: subtitle,
+    openGraph: {
+      images: [
+        "https://res.cloudinary.com/dbscqlwl7/image/upload/v1689923829/thumbnail/Thumbnail_Bootcamp_zupniw.png",
+        ...previousImages,
+      ],
+    },
   };
 }
 
