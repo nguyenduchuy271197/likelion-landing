1756e7c2f8d2251249454b5d6c281a9fe73ae0b3
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-07-31 14:54:19 +0700

    Update page.tsx

--- src/app/courses/[courseSlug]/page.tsx
+++ src/app/courses/[courseSlug]/page.tsx
@@ -111,7 +111,7 @@ export default async function CourseDetail({
                 <CourseWorkshops />
 
                 {/* Showcase */}
-                {/* <CourseProjects /> */}
+                <CourseProjects />
 
                 {/* Reviews */}
                 <CourseReviews />
