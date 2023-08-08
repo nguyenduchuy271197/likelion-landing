c4a91e8e24e8f9b9e0c8eda5508b69b28fc155a4
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-08-08 10:20:27 +0700

    Update CourseHeading.tsx

--- src/components/courses/CourseHeading.tsx
+++ src/components/courses/CourseHeading.tsx
@@ -8,7 +8,7 @@ export default function CourseHeading({
   return (
     <div className="max-w-xl mx-auto mb-12 text-center lg:text-left lg:max-w-none">
       <div>
-        <h1 className="mb-4 text-2xl font-medium sm:text-3xl lg:text-4xl">
+        <h1 className="mb-4 text-2xl font-bold capitalize sm:text-3xl lg:text-4xl">
           {title}
         </h1>
         <p>{subtitle}</p>
