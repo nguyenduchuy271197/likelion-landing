392b215d53a187d77e345e03958941278550470d
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-08-23 09:50:57 +0700

    phone call

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ import Image from "next/image";
 import siteConfig from "@/config/siteConfig";
 import { Metadata } from "next";
 import Head from "next/head";
+import PhoneCall from "@/components/PhoneCall";
 
 // Load body font
 const bodyFont = Roboto({
@@ -68,6 +69,7 @@ export default function RootLayout({
             <Footer />
           </div>
           <Toaster />
+          <PhoneCall />
         </Providers>
 
         {/* <!-- Meta Pixel Code --> */}
