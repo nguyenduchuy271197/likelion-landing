1cfbdae69735255d0c844bd4a96cf8b80341738c
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-09-14 08:38:28 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({
           </div>
           <Toaster />
           <PhoneCall />
-          <TrialPopup />
+          {/* <TrialPopup /> */}
         </Providers>
 
         {/* <!-- Meta Pixel Code --> */}
