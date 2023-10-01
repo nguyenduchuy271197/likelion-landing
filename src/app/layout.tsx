e6611fd41020d5346bdbcbf40a41b3ae88f9e365
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-10-01 12:10:12 +0700

    zalo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -190,6 +190,17 @@ export default function RootLayout({
           `,
           }}
         ></Script>
+
+        {/* Zalo */}
+        <div
+          className="zalo-chat-widget"
+          data-oaid="2343115192114364602"
+          data-welcome-message="Rất vui khi được hỗ trợ bạn!"
+          data-autopopup="1"
+          data-width="350"
+          data-height="420"
+        ></div>
+        <Script src="https://sp.zalo.me/plugins/sdk.js"></Script>
       </body>
     </html>
   );
