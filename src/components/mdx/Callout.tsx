577d8296d327cce5df5e61189e6b8defc65c82cb
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-10-13 16:44:47 +0700

    new blog

--- src/components/mdx/Callout.tsx
+++ src/components/mdx/Callout.tsx
@@ -8,7 +8,7 @@ export default function Callout({
   children: ReactNode;
 }) {
   return (
-    <div className="flex gap-4 p-6 border-l-4 rounded-md sm:p-8 bg-slate-100">
+    <div className="flex gap-4 p-6 border-l-4 rounded-md bg-slate-100">
       <Lightbulb className="w-6 h-6 shrink-0" />
       <div className="font-medium">{children}</div>
     </div>
