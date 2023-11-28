222529d4aa3a58739eb3d2e81f41f3c0b0febb85
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-11-28 15:03:32 +0700

    landing page

--- src/app/(forms)/forms/notion-hack-20-tuoi-va-1000-do-dau-tien/page.tsx
+++ src/app/(forms)/forms/notion-hack-20-tuoi-va-1000-do-dau-tien/page.tsx
@@ -3,6 +3,9 @@ import RegistrationForm from "./_components/RegistrationForm";
 import { Metadata } from "next";
 import Footer from "@/components/layouts/footer/Footer";
 import { Mulish } from "next/font/google";
+import Curriculum from "./_components/Curriculum";
+import { cn } from "@/lib/utils";
+import Features from "./_components/Features";
 const mulishFont = Mulish({
   subsets: ["vietnamese"],
   weight: ["400", "500", "800"],
@@ -15,7 +18,7 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div className={mulishFont.className}>
+    <div className={cn(mulishFont.className)}>
       <div className="relative min-h-screen sm:bg-[url('https://res.cloudinary.com/dbscqlwl7/image/upload/v1700796018/forms/notion-webinar/BG_D_s6zofb.png')] bg-[url('https://res.cloudinary.com/dbscqlwl7/image/upload/v1700796019/forms/notion-webinar/BG_M_tnrqsm.png')] bg-cover bg-center pb-[450px] lg:pb-0">
         <Image
           src="https://res.cloudinary.com/dbscqlwl7/image/upload/v1700796019/forms/notion-webinar/Key_Info_fupvlg.png"
@@ -40,6 +43,10 @@ export default function Page() {
             className="absolute bottom-0 left-0 object-contain lg:hidden"
           />
         </div>
+      </div>
+      <div className="pb-20 space-y-32">
+        <Features />
+        <Curriculum />
       </div>
       <Footer />
     </div>
