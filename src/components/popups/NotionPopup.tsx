df8faea0de5aba7a3640ed13eb4678ae4b249d8d
tuanphucnguyen2110@gmail.com <119840033+Fugena22@users.noreply.github.com>
2024-05-03 11:10:01 +0700

    update:popup

--- src/components/popups/NotionPopup.tsx
+++ src/components/popups/NotionPopup.tsx
@@ -4,21 +4,18 @@ import Image from "next/image";
 import { AspectRatio } from "../ui/AspectRatio";
 import Popup from "./Popup";
 import Link from "next/link";
+import ktc from "/public/assets/Popup.png";
 
 export default function NotionPopup() {
   return (
     <Popup delay={3000}>
       <Link
-        href="https://www.likelion.edu.vn/forms/notion-hack-20-tuoi-va-1000-do-dau-tien"
+        href="https://ktc.likelion.edu.vn/"
         target="_blank"
         className="relative"
       >
         <AspectRatio ratio={1 / 1}>
-          <Image
-            src="https://res.cloudinary.com/dbscqlwl7/image/upload/v1701417893/forms/notion-webinar/Pop_Up_yvj7st.jpg"
-            alt="Notion Workshop | LIKELION"
-            fill
-          />
+          <Image src={ktc} alt="KTC" fill />
         </AspectRatio>
       </Link>
     </Popup>
