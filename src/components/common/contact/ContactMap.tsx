0bef71e4a07d80f726d6ff6fac7eb9c1e6f23307
Huy Nguyen <56339209+nguyenduchuy271197@users.noreply.github.com>
2025-02-24 11:24:14 +0700

    Update ContactMap.tsx

--- src/components/common/contact/ContactMap.tsx
+++ src/components/common/contact/ContactMap.tsx
@@ -3,15 +3,7 @@ import React from "react";
 export default function ContactMap() {
   return (
     <div>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.384990898187!2d106.70361307489534!3d10.781796989367301!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f4ec96fae79%3A0xda53c6fa6c52539a!2sLim%20Tower!5e0!3m2!1sen!2s!4v1688024071298!5m2!1sen!2s"
-        width={600}
-        height={550}
-        allowFullScreen
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-        className="w-full border-0"
-      />
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3751.6077868661287!2d106.7245549!3d10.7308338!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317525f46b232607%3A0x784e860aec9c7081!2sCOBI%20TOWER%202!5e1!3m2!1sen!2s!4v1740371027528!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   );
 }
